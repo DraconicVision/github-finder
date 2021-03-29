41af1f5bf7a392cffe8f8d58e59c473cc4431ede
DraconicVision <46660232+DraconicVision@users.noreply.github.com>
2021-03-29 20:24:17 +0100

    Removed some default files, swapped app.js to class based

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// Doing this ^ rather than the below means that on line 7 I can just do 'Component' rather than React.Component
+// import React, from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components can be functions or classes. This is a class-based component.
+
+class App extends Component {
+  // You need to use a method to return the component. A method is a function within a class.
+  render() {
+    // Render is a lifecycle method. Meaning it runs at a certain point when the component is loading.
+    return (
+      <div className='App'>
+        <h1>Hello from React!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
